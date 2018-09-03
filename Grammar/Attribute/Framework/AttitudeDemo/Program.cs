c6dbed4258a9c12066ca62867f0b2dfdf40100a2
jiangyan <771078740@qq.com>
2018-09-03 16:55:10 +0800

    1.增加特性语法的学习，AttitudeDemo项目为.NET Framework 框架自带特性的学习项目集合

---
+++ Grammar/Attribute/Framework/AttitudeDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttitudeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass.Message("In Main function.");
+            function1();
+            Console.ReadKey();
+        }
+
+        static void function1()
+        {
+            Myclass.Message("In Function 1.");
+            function2();
+        }
+        static void function2()
+        {
+            Myclass.Message("In Function 2.");
+        }
+    }
+
+    public class Myclass
+    {
+        [Conditional("DEBUG")]//仅限在Debug调试模式下有效特性，Release下则无效；姜彦201809031652
+        public static void Message(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
