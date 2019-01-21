e8d8df633ac6ed15db1b0d930fe1c97e887d86f2
jiangyan <771078740@qq.com>
2019-01-21 16:57:28 +0800

    1.对浅克隆跟深克隆进行了分类说明

--- Grammar/Clone/Clone/CloneExtends.cs
+++ Grammar/Clone/Clone/CloneExtends.cs
@@ -31,6 +31,7 @@ namespace Clone
     /// </summary>
     public static class CloneExtends
     {
+        #region 浅克隆
         /// <summary>
         /// 对象浅克隆;无法克隆对象中的引用类型
         /// </summary>
@@ -59,27 +60,11 @@ namespace Clone
             return model;
         }
 
-        /// <summary>
-        /// 对象深度克隆;同时克隆对象中的引用类型;对象必须被序列化过才行;
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="t"></param>
-        /// <returns></returns>
-        public static T DeepCloneObject<T>(this T t) where T : class
-        {
-            T model = null;
-            using (MemoryStream memoryStream = new MemoryStream())
-            {
-                BinaryFormatter binaryFormatter = new BinaryFormatter();
-                binaryFormatter.Serialize(memoryStream, t);
-                memoryStream.Position = 0;
-                model = (T)binaryFormatter.Deserialize(memoryStream);
-            }
-            return model;
-        }
+        
 
         /// <summary>
-        /// 对象列表深度克隆
+        /// 对象列表浅克隆
+        /// 这个方法实际上只能完成对List的浅克隆
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="tList"></param>
@@ -110,7 +95,31 @@ namespace Clone
             }
             return listNew;
         }
+        #endregion
 
+        #region 深克隆
+
+        /// <summary>
+        /// 对象深度克隆;同时克隆对象中的引用类型;对象必须被序列化过才行;
+        /// 同时支持对List的深度克隆，也是必须序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static T DeepCloneObject<T>(this T t) where T : class
+        {
+            T model = null;
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(memoryStream, t);
+                memoryStream.Position = 0;
+                model = (T)binaryFormatter.Deserialize(memoryStream);
+            }
+            return model;
+        }
+
+        #endregion
         /// <summary>
         /// 获取对象的改变状态list
         /// </summary>
