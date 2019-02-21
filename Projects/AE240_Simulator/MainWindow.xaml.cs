86b84945342a45bf4f197da66ae035024ff546a9
jiangyan <771078740@qq.com>
2019-02-21 17:29:42 +0800

    feat:1.研磨接口与抽象类的区别与联系

--- Projects/AE240_Simulator/MainWindow.xaml.cs
+++ Projects/AE240_Simulator/MainWindow.xaml.cs
@@ -1,23 +1,9 @@
-﻿using AE240_Simulator.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Utility.Circuit.Controller;
 
 namespace AE240_Simulator
-{   
-
+{
+#pragma warning disable CS1591
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
@@ -33,4 +19,64 @@ namespace AE240_Simulator
 
    
     }
+
+    /*接口跟抽象类的区别与联系*/
+    
+    public enum AllTestType:int
+    {       
+        Normal=0,       
+        Err=1,
+    }
+
+    public delegate void OnExceptionedEventHandler(object sender,EventArgs e);
+   
+
+    interface IContent
+    {
+        int Add(int a, int b);
+        
+        event OnExceptionedEventHandler OnExceptioned;     
+        string Age { get; set; }   
+        AllTestType TestType { get; set; }
+        /*
+         * 接口无法声明类型：委托
+         * 接口不能包含字段：静态、动态、常量、字符串、整型、枚举等等
+         * 接口中的方法、事件、属性不需要修饰符
+         */
+        //接口无法声明类型：委托
+        //delegate void OnConnectedEventHandler();
+
+        //接口不能包含字段
+        //const uint SENSOR_RT = 0x64010101;
+
+        //接口不能包含字段
+        //string Name;
+
+        //接口不能包含字段
+        //AllTestType _TestType;
+
+        //接口不能包含字段
+        //static int TotalCount = 100;
+    }
+
+    abstract class AbstractContent
+    {
+        public abstract int Add(int a, int b);        
+
+        public  event OnExceptionedEventHandler OnExceptioned;
+        public abstract Delegate GetDelegate();
+        public string Age { get; set; }    
+        public AllTestType TestType { get; set; }
+        /*
+         * 抽象类可以声明类型：委托
+         * 抽象类能够包含字段：静态、动态、常量、字符串、整型、枚举等等
+         * 抽象类中的方法、事件、属性不需要修饰符
+         */
+        public delegate void OnConnectedEventHandler();
+        public const uint SENSOR_RT = 0x64010101;
+        public string Name;
+        public AllTestType _TestType;
+        public static int TotalCount = 100;
+    }
+
 }
